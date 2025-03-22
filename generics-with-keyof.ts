561b2c0d94ed890071308eacffe2a63e5c53467f
rashmipullur <mheresince1998@gmail.com>
2025-03-22 21:45:04 +0530

    generics with key of

---
+++ generics-with-keyof.ts
@@ -0,0 +1,28 @@
+function pluck<DataType, KeyType extends keyof DataType>(items: DataType[], key: KeyType): DataType[KeyType][] {
+    return items.map(item => item[key])
+}
+
+const dogs = [
+    {name: "Rosko", age: 12},
+    {name: "Leo", age: 10}
+]
+
+console.log(pluck(dogs, "age"))
+console.log(pluck(dogs, "name"))
+
+interface BaseEvent {
+    time: number;
+    user: string
+}
+
+interface EventMap {
+    addToCart: BaseEvent & { quantity: number; productID: string },
+    checkout: BaseEvent
+}
+
+function sendEvent<Name extends keyof EventMap>(name: Name, data: EventMap[Name]): void {
+    console.log([name, data])
+}
+
+sendEvent("addToCart", {productID: 'foo', user: "baz", quantity: 1, time: 10})
+sendEvent("checkout", {time: 20, user: 'bob'})
